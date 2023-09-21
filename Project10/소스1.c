4f58138055dab2f7d7a5326b96294e79abb0f2a5
Jeongwon Park <j2982477j@gmail.com>
2023-09-21 19:12:00 +0900

    3주차 - bubble sort

--- Project10/소스1.c
+++ Project10/소스1.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 int main() {
-	int n, i, left, right, mid, *a, key;
-	printf("Enter Array Size : ");
+	int n, i, j, temp,*a;
+	printf("Enter Array Size: ");
 	scanf("%d", &n);
-
 	a = (int*)malloc(n * sizeof(int));
-
-	printf("Enter %d numbers in sorted order: ", n);
+	printf("Input: number [%d]: ", n);
 	for (i = 0; i < n; i++)
 		scanf("%d", &a[i]);
-	left = 0, right = n - 1, mid = (left + right) / 2;
-	printf("Enter search Key: ");
-	scanf("%d", &key);
-	while (left <= right) {
-		if (key < a[mid])
-			right = mid - 1;
-		else if (key > a[mid])
-			left = mid + 1;
-		else {
-			printf("Found at Location %d\n", mid);
-			break;
+
+
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n - 1; j++) {
+			if (a[j] < a[j + 1]) {
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
 		}
-		mid = (left + right) / 2;
 	}
-	if (left > right)
-		printf("Not Found\n");
+	printf("Output: \n");
+	for (i = 0; i < n; i++) {
+		printf("%d: %d\n",i, a[i]);
+	}
+	printf("largest element: %d\n", a[0]);
+	printf("smallest element: %d\n", a[n-1]);
+	free(a);
 	return 0;
 }
