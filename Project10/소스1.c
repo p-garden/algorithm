7ea51d8456da1b3bce1c02fe66ef8447a8a64eab
Jeongwon Park <j2982477j@gmail.com>
2023-10-21 02:03:36 +0900

    merge sort 내가짬

--- Project10/소스1.c
+++ Project10/소스1.c
@@ -1,70 +1,56 @@
-#include <stdio.h>
-// Function to swap the position of two elements
-void swap(int* a, int* b)
-{
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
-// To heapify a subtree rooted with node i
-// which is an index in arr[].
-// n is size of heap
-void heapify(int arr[], int N, int i)
-{
-	// Find largest among root,
-	// left child and right child
-	// Initialize largest as root
-	int largest = i;
-	// left = 2*i + 1
-	int left = 2 * i + 1;
-	// right = 2*i + 2
-	int right = 2 * i + 2;
-	// If left child is larger than root
-	if (left < N && arr[left] > arr[largest])
-		largest = left;
-	// If right child is larger than largest
-	// so far
-	if (right < N && arr[right] > arr[largest])
-		largest = right;
-	// Swap and continue heapifying
-	// if root is not largest
-	// If largest is not root
-	if (largest != i) {
-		swap(&arr[i], &arr[largest]);
-		// Recursively heapify the affected
-		// sub-tree
-		heapify(arr, N, largest);
+#include<stdio.h>
+#include<stdlib.h>
+void merge(int arr[], int l,int m,int r) {
+	int a1, a2,i,j,k; 
+	a1 = m - l + 1;
+	a2 = r - m;
+	int *L1, *R1;
+	L1 = (int*)malloc(a1 * sizeof(int));
+	R1 = (int*)malloc(a2 * sizeof(int));
+	for (i = 0; i < a1; i++) 
+		L1[i] = arr[i + l];
+	for (j = 0; j < a2; j++)
+		R1[j] = arr[m + 1 + j];
+	i = 0, j = 0,k = l;
+	while (i < a1 && j < a2) {
+		if (L1[i] <= R1[j]) {
+			arr[k] = L1[i];
+			i++;
+		}
+		else  {
+			arr[k] = R1[j];
+			j++;
+		}
+		k++;
+	}
+	while (i < a1) {
+		arr[k] = L1[i];
+		i++,k++;
+	}
+	while (j < a2) {
+		arr[k] = R1[j];
+		j++, k++;
 	}
 }
-// Main function to do heap sort
-void heapSort(int arr[], int N)
-{
-	// Build max heap
-	for (int i = N / 2 - 1; i >= 0; i--)
-		heapify(arr, N, i);
-	// Heap sort
-	for (int i = N - 1; i >= 0; i--) {
-		swap(&arr[0], &arr[i]);
-		// Heapify root element
-		// to get highest element at
-		// root again
-		heapify(arr, i, 0);
+void merge_sort(int arr[], int l, int r) {
+	int m;
+	if (l < r) {
+		m = l + (r-l)/2; 
+		merge_sort(arr, l, m);
+		merge_sort(arr, m + 1, r);
+		merge(arr, l, m, r);
 	}
 }
-// A utility function to print array of size n
-void printArray(int arr[], int N)
-{
-	for (int i = 0; i < N; i++)
+int main() {
+	int key,i,j,temp,min_idx;
+	int n = 5;
+	int arr[5] = {1,7,2,6,9 };
+
+	merge_sort(arr, 0, n-1);
+
+
+	
+	for (i = 0; i < n; i++)
 		printf("%d ", arr[i]);
-	printf("\n");
-}
-// Driver's code
-int main()
-{
-	int arr[] = { 12, 11, 13, 5, 6, 7 };
-	int N = sizeof(arr) / sizeof(arr[0]);
-	// Function call
-	heapSort(arr, N);
-	printf("Sorted array is\n");
-	printArray(arr, N);
+		
 }
